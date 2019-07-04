4c62079188aaf46499ab4183047988165e23eca2
Alan Krajina <alankrajina88@gmail.com>
2019-07-04 09:02:45 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -40,9 +40,7 @@ class Application
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
-    elsif req.path.match(/search/)
-      search_term = req.params["q"]
-      resp.write handle_search(search_term)
+
     else
       resp.write "Your cart is empty"
     end
@@ -50,13 +48,8 @@ class Application
     resp.finish
   end
 
-  def handle_search(search_term)
-    if @@cart.include?(search_term)
-      return "#{search_term} is one of our items"
-    else
-      return "Couldn't find #{search_term}"
-    end
-  end
+
+  
   
   
 end
