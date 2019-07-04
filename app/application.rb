c7461984bc84d809f45ce74878de3ccfd89095d9
Alan Krajina <alankrajina88@gmail.com>
2019-07-04 09:07:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -36,7 +36,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/cart/)
+    if @@cart
       @@cart.each do |item|
         resp.write "#{item}\n"
       end
